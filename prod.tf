7cd6a2f5ced5b089631125fe67cb325f682bf0f9
sathiyajith <sathiyajith19@gmail.com>
2021-08-18 14:58:46 +0530

    Used EIP Association and scaled the instance

--- prod.tf
+++ prod.tf
@@ -42,6 +42,8 @@ resource "aws_security_group" "prod_web" {
 }
 
 resource "aws_instance" "prod_web" {
+  count = 2
+
   ami = "ami-0afca6c808e56d77d"
   instance_type = "t2.nano"
 
@@ -54,8 +56,12 @@ resource "aws_instance" "prod_web" {
   }
 }
 
+resource "aws_eip_association" "prod_web" {
+  instance_id = "aws_instance.prod_web.0.id"
+  allocation_id = "aws_eip.prod_web.id"
+}
+
 resource "aws_eip" "prod_web" {
-  instance = aws_instance.prod_web.id
 
    tags = {
      "Terraform" : "true"
