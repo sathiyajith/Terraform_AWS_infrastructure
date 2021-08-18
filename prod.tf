1267789762315f0195522cfcd6941c14244c98d4
sathiyajith <sathiyajith19@gmail.com>
2021-08-18 17:54:57 +0530

    Load Balancer added

--- prod.tf
+++ prod.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   profile = "default"
-  region  =  "us-west-2"
+  region  =  "us-east-2"
 }
 
 
@@ -10,6 +10,20 @@ resource "aws_s3_bucket" "prod_learning_tf" {
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-east-2a"
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "us-east-2b"
+  tags = {
+    "Terraform" : "true"
+  }
+}
 
 resource "aws_security_group" "prod_web" {
   name = "prod_web"
@@ -44,7 +58,7 @@ resource "aws_security_group" "prod_web" {
 resource "aws_instance" "prod_web" {
   count = 2
 
-  ami = "ami-0afca6c808e56d77d"
+  ami = "ami-00aa7ba95db396d37"
   instance_type = "t2.nano"
 
   vpc_security_group_ids = [
@@ -56,14 +70,35 @@ resource "aws_instance" "prod_web" {
   }
 }
 
-resource "aws_eip_association" "prod_web" {
-  instance_id = "aws_instance.prod_web.0.id"
-  allocation_id = "aws_eip.prod_web.id"
-}
-
 resource "aws_eip" "prod_web" {
 
    tags = {
      "Terraform" : "true"
    }
 }
+
+resource "aws_eip_association" "prod_web" {
+  instance_id = aws_instance.prod_web.0.id
+  allocation_id = aws_eip.prod_web.id
+}
+
+
+resource "aws_elb" "prod_web" {
+  name = "prod-web"
+  instances = aws_instance.prod_web.*.id
+  subnets = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  security_groups = [aws_security_group.prod_web.id]
+  
+  tags = {
+    "Terraform" : "true"
+  }
+
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+}
+
+
