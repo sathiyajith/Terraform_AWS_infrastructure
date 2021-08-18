686605f922fe953d47f57920e8f1911b62692c84
sathiyajith <sathiyajith19@gmail.com>
2021-08-18 19:49:04 +0530

    Refactored to use ASG

--- prod.tf
+++ prod.tf
@@ -55,37 +55,41 @@ resource "aws_security_group" "prod_web" {
   }
 }
 
-resource "aws_instance" "prod_web" {
-  count = 2
-
-  ami = "ami-00aa7ba95db396d37"
+resource "aws_launch_template" "prod_web" {
+  name_prefix = "prod-web"
+  image_id = "ami-00aa7ba95db396d37"
   instance_type = "t2.nano"
-
-  vpc_security_group_ids = [
-    aws_security_group.prod_web.id
-  ]
-
-  tags = {
-    "Terraform" : "true"
-  }
-}
-
-resource "aws_eip" "prod_web" {
-
    tags = {
      "Terraform" : "true"
    }
+
 }
 
-resource "aws_eip_association" "prod_web" {
-  instance_id = aws_instance.prod_web.0.id
-  allocation_id = aws_eip.prod_web.id
+resource "aws_autoscaling_group" "prod_web" {
+  min_size = 1
+  max_size = 2
+  vpc_zone_identifier = [aws_default_subnet.default_az1.id,aws_default_subnet.default_az2.id]
+
+  launch_template {
+    id = aws_launch_template.prod_web.id
+  }
+
+  tag{
+    key = "Terraform"
+    value = "true"
+    propagate_at_launch = true
+  }
 }
+
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb = aws_elb.prod_web.id
+}
+
 
 
 resource "aws_elb" "prod_web" {
   name = "prod-web"
-  instances = aws_instance.prod_web.*.id
   subnets = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
   security_groups = [aws_security_group.prod_web.id]
   
